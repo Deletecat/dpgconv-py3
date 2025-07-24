3adcbf064365e037b1d40e5e1e9986673cde2a75
Deletecat <kit@deletecat.com>
2025-07-24 21:30:05 +0100

    initial python3 port
    The script doesn't appear to work in it's current state.
    But it will try to run in py3 at least.

--- dpgconv.py
+++ dpgconv.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # -*- coding: utf-8-*-
 #
 # DPG Converter for linux
@@ -96,12 +96,16 @@ command line options:
 
 """
 
-import sys, os, optparse
+import sys
+import os
 from PIL import Image
 import tempfile
 from optparse import OptionParser
 import signal
-import commands,re,stat,struct,subprocess
+import re
+import stat
+import struct
+import subprocess
 
 MP2TMP="mp2tmp.mp2"
 MPGTMP="mpgtmp.mpg"
@@ -119,25 +123,25 @@ MPEG_STAT="mpeg_stat"
 
 #Print a help message if requested.
 if "-h" in sys.argv or "-help" in sys.argv or "--help" in sys.argv:
-	print __doc__
+	print(__doc__)
 	raise SystemExit
 
 def cleanup_callback(a,b):
-	print "Removing temporary files"
-	if os.path.lexists ( MPGTMP ):
-		os.unlink ( MPGTMP )
-	if os.path.lexists ( MP2TMP ):
-		os.unlink ( MP2TMP )
-	if os.path.lexists ( HEADERTMP ):
-		os.unlink ( HEADERTMP )
-	if os.path.lexists ( GOPTMP ):
-		os.unlink ( GOPTMP )
-	if os.path.lexists ( STATTMP ):
-		os.unlink ( STATTMP )
-	if os.path.lexists ( THUMBTMP ):
-		os.unlink ( THUMBTMP )
-	if os.path.lexists ( SHOTTMP ):
-		os.rmdir ( SHOTTMP )
+	print("Removing temporary files")
+	if os.path.lexists(MPGTMP):
+		os.unlink(MPGTMP)
+	if os.path.lexists(MP2TMP):
+		os.unlink(MP2TMP)
+	if os.path.lexists(HEADERTMP):
+		os.unlink(HEADERTMP)
+	if os.path.lexists(GOPTMP):
+		os.unlink(GOPTMP)
+	if os.path.lexists(STATTMP):
+		os.unlink(STATTMP)
+	if os.path.lexists(THUMBTMP):
+		os.unlink(THUMBTMP)
+	if os.path.lexists(SHOTTMP):
+		os.rmdir(SHOTTMP)
 
 def conv_vid(file):
 	if ((options.dpg == 0) | (options.dpg == 4)):
@@ -156,11 +160,11 @@ def conv_vid(file):
 		options.pf = 3
 
 	if options.aspect:
-		aspect = commands.getoutput( MPLAYER + " -frames 1 -vo null -ao null -identify \"" + file + "\" | grep -E \"^ID_VIDEO_ASPECT\" | tail -1")
+		aspect = subprocess.getoutput(f'{MPLAYER} -frames 1 -vo null -ao null -identify "{file}" | grep -E "^ID_VIDEO_ASPECT" | tail -1')
 		p = re.compile ("ID_VIDEO_ASPECT=(.*)")
-		m = p.search( aspect )
+		m = p.search(aspect)
 		if m:
-			print "Aspect ratio = %s" % m.group(1)
+			print(f"Aspect ratio = {m.group(1)}")
 			ar = float(m.group(1))
 			if int(256.0/ar) <= 192:
 				options.width=256
@@ -168,48 +172,48 @@ def conv_vid(file):
 			else:
 				options.height=192
 				options.width=int(ar*192.0)
-			print "Target size set to %dx%d." % (options.width,options.height)
+			print(f"Target size set to {options.width}x{options.height}.")
 
 	if options.tp:
 		if options.fps < 24:
-			print "mencoder won't work with double pass and fps < 24, forcing fps = 24"
+			print("mencoder won't work with double pass and fps < 24, forcing fps = 24")
 			options.fps = 24
-		v_cmd =  ( " \""+ file +"\" -v -ofps " + `options.fps` + " -sws 9 -vf " + v_pf + "scale=" + `options.width` + ":" + `options.height` +":::3,harddup -nosound -ovc lavc -lavcopts vcodec=mpeg1video:vstrict=-2:mbd=2:trell:mv0:vmax_b_frames=2:cmp=6:subcmp=6:precmp=6:dia=4:predia=4:bidir_refine=4:mv0_threshold=0:last_pred=3:vbitrate=" + `options.vbps` )
+		v_cmd = f' "{file}" -v -ofps {options.fps} -sws 9 -vf {v_pf} scale={options.width}:{options.height}:::3,harddup -nosound -ovc lavc -lavcopts vcodec=mpeg1video:vstrict=-2:mbd=2:trell:mv0:vmax_b_frames=2:cmp=6:subcmp=6:precmp=6:dia=4:predia=4:bidir_refine=4:mv0_threshold=0:last_pred=3:vbitrate={options.vbps}'
 	elif options.hq:
-		v_cmd =  ( " \""+ file +"\" -v -ofps " + `options.fps` + " -sws 9 -vf " + v_pf + "scale=" + `options.width` + ":" + `options.height` +":::3,harddup -nosound -ovc lavc -lavcopts vcodec=mpeg1video:vstrict=-2:mbd=2:trell:mv0:cmp=6:subcmp=6:precmp=6:dia=3:predia=3:last_pred=3:vbitrate=" + `options.vbps` + " -o " + MPGTMP + " -of rawvideo" )
+		v_cmd = f' "{file}" -v -ofps {options.fps} -sws 9 -vf {v_pf} scale={options.width}:{options.height}:::3,harddup -nosound -ovc lavc -lavcopts vcodec=mpeg1video:vstrict=-2:mbd=2:trell:mv0:cmp=6:subcmp=6:precmp=6:dia=3:predia=3:last_pred=3:vbitrate={options.vbps} -o {MPGTMP} -of rawvideo'
 	elif options.lq:
-		v_cmd = ( " \"" + file + "\" -v -ofps " + `options.fps` + " -vf " + v_pf + "scale=" + `options.width` + ":" + `options.height` + ",harddup -nosound -ovc lavc -lavcopts vcodec=mpeg1video:vstrict=-2:vbitrate=" + `options.vbps` + " -o " + MPGTMP + " -of rawvideo" )
+		v_cmd = f' "{file}" -v -ofps {options.fps} -vf {v_pf} scale={options.width}:{options.height},harddup -nosound -ovc lavc -lavcopts vcodec=mpeg1video:vstrict=-2:vbitrate={options.vbps} -o {MPGTMP} -of rawvideo'
 	else :
-		v_cmd = ( " \""+ file +"\" -v -ofps " + `options.fps` + " -sws 9 -vf " + v_pf + "scale=" + `options.width` + ":" + `options.height` + ":::3,harddup -nosound -ovc lavc -lavcopts vcodec=mpeg1video:vstrict=-2:mbd=2:trell:mv0:cmp=2:subcmp=2:precmp=2:vbitrate=" + `options.vbps` + " -o " + MPGTMP + " -of rawvideo")
+		v_cmd = f' "{file}" -v -ofps {options.fps} -sws 9 -vf {v_pf} scale={options.width}:{options.height}:::3,harddup -nosound -ovc lavc -lavcopts vcodec=mpeg1video:vstrict=-2:mbd=2:trell:mv0:cmp=2:subcmp=2:precmp=2:vbitrate={options.vbps} -o {MPGTMP} -of rawvideo'
 	
 	if options.nosub:
-		if options.sub != None:
-			v_cmd = " -sub \"" + options.sub + "\" " + v_cmd
+		if options.sub is not None:
+			v_cmd = f'" -sub "{options.sub}" {v_cmd}'
 	else:
 		basename = os.path.splitext ( file )[0]
-		if options.sid != None:
-			v_cmd = " -sid \"" + str(options.sid) + "\" " + v_cmd
-		if options.sub != None:
-			v_cmd = " -sub \"" + options.sub + "\" " + v_cmd
+		if options.sid is not None:
+			v_cmd = f' -sid "{str(options.sid)}" {v_cmd}'
+		if options.sub is not None:
+			v_cmd = f' -sub "{options.sub}" {v_cmd}'
 		elif os.path.exists ( basename + ".ass" ):
-			v_cmd = " -sub \"" + basename + ".ass" + "\" " + v_cmd
+			v_cmd = f' -sub "{basename}.ass" {v_cmd}'
 		elif os.path.exists ( basename + ".srt" ):
-			v_cmd = " -sub \"" + basename + ".srt" + "\" " + v_cmd
+			v_cmd = f' -sub "{basename}.srt" {v_cmd}'
 		elif os.path.exists ( basename + ".sub" ):
-			v_cmd = " -sub \"" + basename + ".sub" + "\" " + v_cmd
+			v_cmd = f' -sub "{basename}.sub" {v_cmd}'
 		elif os.path.exists ( basename + ".ssa" ):
-			v_cmd = " -sub \"" + basename + ".ssa" + "\" " + v_cmd
+			v_cmd = f' -sub "{basename}.ssa" {v_cmd}'
 	
-	if options.subcp != None:
-		v_cmd = " -subcp " + options.subcp + v_cmd
-	if options.font != None:
-		v_cmd = " -font \"" + options.font + "\"" + v_cmd
+	if options.subcp is not None:
+		v_cmd = f' -subcp {options.subcp} {v_cmd}'
+	if options.font is not None:
+		v_cmd = f' -font "{options.font}" {v_cmd}'
 
 	v_cmd = MENCODER + " " + v_cmd
 	if options.tp:
 		v_cmd_two = v_cmd
-		v_cmd = v_cmd + ":vpass=1:turbo:vb_strategy=2:vrc_maxrate=500:vrc_minrate=0:vrc_buf_size=327:intra_matrix=8,9,12,22,26,27,29,34,9,10,14,26,27,29,34,37,12,14,18,27,29,34,37,38,22,26,27,31,36,37,38,40,26,27,29,36,39,38,40,48,27,29,34,37,38,40,48,58,29,34,37,38,40,48,58,69,34,37,38,40,48,58,69,79:inter_matrix=16,18,20,22,24,26,28,30,18,20,22,24,26,28,30,32,20,22,24,26,28,30,32,34,22,24,26,30,32,32,34,36,24,26,28,32,34,34,36,38,26,28,30,32,34,36,38,40,28,30,32,34,36,38,42,42,30,32,34,36,38,40,42,44" + " -o " + MPGTMP + " -of rawvideo"
-		v_cmd_two = v_cmd_two + ":vpass=2:vrc_maxrate=500:vrc_minrate=0:vrc_buf_size=327:keyint=60:intra_matrix=8,9,12,22,26,27,29,34,9,10,14,26,27,29,34,37,12,14,18,27,29,34,37,38,22,26,27,31,36,37,38,40,26,27,29,36,39,38,40,48,27,29,34,37,38,40,48,58,29,34,37,38,40,48,58,69,34,37,38,40,48,58,69,79:inter_matrix=16,18,20,22,24,26,28,30,18,20,22,24,26,28,30,32,20,22,24,26,28,30,32,34,22,24,26,30,32,32,34,36,24,26,28,32,34,34,36,38,26,28,30,32,34,36,38,40,28,30,32,34,36,38,42,42,30,32,34,36,38,40,42,44" + " -o " + MPGTMP + " -of rawvideo"
+		v_cmd = f'{v_cmd} :vpass=1:turbo:vb_strategy=2:vrc_maxrate=500:vrc_minrate=0:vrc_buf_size=327:intra_matrix=8,9,12,22,26,27,29,34,9,10,14,26,27,29,34,37,12,14,18,27,29,34,37,38,22,26,27,31,36,37,38,40,26,27,29,36,39,38,40,48,27,29,34,37,38,40,48,58,29,34,37,38,40,48,58,69,34,37,38,40,48,58,69,79:inter_matrix=16,18,20,22,24,26,28,30,18,20,22,24,26,28,30,32,20,22,24,26,28,30,32,34,22,24,26,30,32,32,34,36,24,26,28,32,34,34,36,38,26,28,30,32,34,36,38,40,28,30,32,34,36,38,42,42,30,32,34,36,38,40,42,44 -o {MPGTMP} -of rawvideo'
+		v_cmd_two = f'{v_cmd_two} :vpass=2:vrc_maxrate=500:vrc_minrate=0:vrc_buf_size=327:keyint=60:intra_matrix=8,9,12,22,26,27,29,34,9,10,14,26,27,29,34,37,12,14,18,27,29,34,37,38,22,26,27,31,36,37,38,40,26,27,29,36,39,38,40,48,27,29,34,37,38,40,48,58,29,34,37,38,40,48,58,69,34,37,38,40,48,58,69,79:inter_matrix=16,18,20,22,24,26,28,30,18,20,22,24,26,28,30,32,20,22,24,26,28,30,32,34,22,24,26,30,32,32,34,36,24,26,28,32,34,34,36,38,26,28,30,32,34,36,38,40,28,30,32,34,36,38,42,42,30,32,34,36,38,40,42,44 -o {MPGTMP} -of rawvideo'
 		v_cmd_two = v_cmd_two + " " + options.mv
 		v_cmd = v_cmd + " " + options.mv
 	else:
@@ -218,66 +222,63 @@ def conv_vid(file):
 	#print v_cmd_two
 	proc = subprocess.Popen(v_cmd,shell=True,stdout=subprocess.PIPE,universal_newlines=True,stderr=open('/dev/null', 'w'))
 	
-	p = re.compile ("f (\(.*%\))")
+	p = re.compile (r"f (\(.*%\))")
 	for line in proc.stdout:
 		m = p.search( line )
 		#print line
 		if m:
-			print "Transcoding video: " + m.group(1) + "\r" ,
-	print "Transcoding video:   done"
+			print("Transcoding video: " + m.group(1) + "\r", end=" ")
+	print("Transcoding video:   done")
 	if options.tp:
 		proc = subprocess.Popen(v_cmd_two,shell=True,stdout=subprocess.PIPE,universal_newlines=True,stderr=open('/dev/null', 'w'))
 		for line in proc.stdout:
 			m = p.search( line )
 		
 			if m:
-				print "Transcoding video, pass 2: " + m.group(1) + "\r" ,
-		print "Transcoding video, pass 2:   done"
+				print("Transcoding video, pass 2: " + m.group(1) + "\r", end=" ")
+		print("Transcoding video, pass 2:   done")
 
 
 def conv_aud(file):
 	vol=''
 	if options.volnorm:
 		vol="-af volnorm"
-
-	a_cmd = ( MENCODER + " \"" +file + "\" -v -of rawaudio -oac lavc -ovc copy -lavcopts acodec=mp2:abitrate=" + `options.abps` +" -o " + MP2TMP + " "+vol)
-	identify = commands.getoutput( MPLAYER + " -frames 0 -vo null -ao null -identify \"" + file + "\" | grep -E \"^ID|VIDEO|AUDIO\"")
-	p = re.compile ("([0-9]*)( ch)")
-	m = p.search( identify )
+	a_cmd = f'{MENCODER} "{file}" -v -of rawaudio -oac lavc -ovc copy -lavcopts acodec=mp2:abitrate={options.abps} -o {MP2TMP} {vol}'
+	identify = subprocess.getoutput(MPLAYER + " -frames 0 -vo null -ao null -identify \"" + file + "\" | grep -E \"^ID|VIDEO|AUDIO\"")
+	p = re.compile("([0-9]*)( ch)")
+	m = p.search(identify)
 	if m:
-		c = m.group(1)
-		if options.channels == None:
+		c = int(m.group(1))
+		if options.channels is None:
 			if c > 2:
-				a_cmd = a_cmd + " -af channels=2,resample=" +`options.hz`+ ":1:2"
+				a_cmd = f'{a_cmd} -af channels=2,resample={options.hz}:1:2'
 			else:
-				a_cmd = a_cmd + " -af resample=" +`options.hz`+ ":1:2"
+				a_cmd = f'{a_cmd} -af resample={options.hz}:1:2'
 		elif options.channels >= 2:
-			a_cmd = a_cmd + " -af channels=2,resample=" +`options.hz`+ ":1:2"
+			a_cmd = f'{a_cmd} -af channels=2,resample={options.hz}:1:2'
 		else:
-			a_cmd = a_cmd + " -af channels=1,resample=" +`options.hz`+ ":1:2"
+			a_cmd = f'{a_cmd} -af channels=1,resample={options.hz}:1:2'
 	else:
-		print "Error running mplayer:"
-		print identify
+		print("Error running mplayer:")
+		print(identify)
 
-	if options.aid != None:
+	if options.aid is not None:
 		a_cmd = a_cmd + " -aid " + str(options.aid)
 
 	a_cmd = a_cmd + " " + options.ma
 	#print a_cmd
 
 	proc = subprocess.Popen(a_cmd,shell=True,stdout=subprocess.PIPE,universal_newlines=True,stderr=subprocess.STDOUT)
-
-	v_out = ""
 	
-	p = re.compile ("f (\(.*%\))")
+	p = re.compile (r"f (\(.*%\))")
 	for line in proc.stdout:
 		m = p.search( line )
 		if m:
-			print "Transcoding audio: " + m.group(1) + "\r" ,
-	print "Transcoding audio:   done"
+			print("Transcoding audio: " + m.group(1) + "\r", end=" ")
+	print("Transcoding audio:   done")
 
 def write_header(frames):
-	print "Creating header"
+	print("Creating header")
 
 	audiostart=36
 	if options.dpg == 1:
@@ -291,7 +292,7 @@ def write_header(frames):
 	videostart = audiostart + audiosize
 	videoend = videostart + videosize
 	f=open(HEADERTMP, 'wb')
-	DPG = "DPG" + `options.dpg`
+	DPG = f'DPG{options.dpg}'
 	headerValues = [ DPG, int(frames), options.fps, 0, options.hz , 0 ,int(audiostart), int(audiosize), int(videostart), int(videosize) ]
 	
 	f.write (struct.pack( "4s" , headerValues[0]))
@@ -316,8 +317,8 @@ def write_header(frames):
 	f.close()
 
 def mpeg_stat():
-	p = re.compile ("frames: ([0-9]*)\.")
-	s_out = commands.getoutput( MPEG_STAT + " -offset " + STATTMP + " " + MPGTMP )
+	p = re.compile (r"frames: ([0-9]*)\.")
+	s_out = subprocess.getoutput( MPEG_STAT + " -offset " + STATTMP + " " + MPGTMP )
 	m = p.search( s_out )
 	if m:
 		frames = m.group(1)
@@ -335,19 +336,19 @@ def mpeg_stat():
 			gop.close()
 			stat.close()
 	else:
-		print s_out
+		print(s_out)
 		return 0
 	return frames
 
 def conv_file(file):
 	if not (os.path.lexists ( file )):
-		print "File " + file + " doesn't exist"
-	print "Converting " + file
+		print("File " + file + " doesn't exist")
+	print("Converting " + file)
 	conv_vid (file)
 	conv_aud(file)
 	frames = mpeg_stat()
 	if frames == 0:
-		print "Error using mpeg_stat ... see error above"
+		print("Error using mpeg_stat ... see error above")
 		cleanup_callback (0,0)
 		return
 	if options.dpg == 4:
@@ -355,19 +356,19 @@ def conv_file(file):
 	write_header(frames)
 	dpgname = os.path.basename ( os.path.splitext ( file )[0] ) + ".dpg"
 	
-	print "Creating " + dpgname
-	#commands.getoutput( "cat \"" + HEADERTMP + "\" \"" + MP2TMP + "\" \"" + MPGTMP + "\" > \"" + dpgname + "\"")
+	print("Creating " + dpgname)
+	#subprocess.getoutput( "cat \"" + HEADERTMP + "\" \"" + MP2TMP + "\" \"" + MPGTMP + "\" > \"" + dpgname + "\"")
 	
 	if options.dpg == 4:
 		concat(dpgname,HEADERTMP,THUMBTMP,MP2TMP,MPGTMP,GOPTMP)
 	elif (options.dpg == 2) | (options.dpg == 3):
-		#commands.getoutput( "cat \"" + GOPTMP + "\" >> \"" + dpgname + "\"")
+		#subprocess.getoutput( "cat \"" + GOPTMP + "\" >> \"" + dpgname + "\"")
 		concat(dpgname,HEADERTMP,MP2TMP,MPGTMP,GOPTMP)
 	else:
 		concat(dpgname,HEADERTMP,MP2TMP,MPGTMP)
 	
 	cleanup_callback (0,0)
-	print "Done converting \"" + file + "\" to \"" + dpgname + "\""
+	print("Done converting \"" + file + "\" to \"" + dpgname + "\"")
 
 def conv_thumb(file, frames):
 	"""Converts PIL internal (24 or 32bit per pixel RGB) image
@@ -376,12 +377,12 @@ def conv_thumb(file, frames):
 	"""
 	shot_file = None
 	if not (os.path.lexists ( file )):
-		print "Preview file will be generated from video file."
+		print("Preview file will be generated from video file.")
 		shot_file = SHOTTMP +"/00000001.png"
 		s_cmd = '%s %s -nosound -vo png:outdir=%s -frames 1 -ss %d' % (
 			MPLAYER, MPGTMP, SHOTTMP, 
 			int((int(frames)/options.fps)/10))
-		output = commands.getoutput(s_cmd)
+		output = subprocess.getoutput(s_cmd)
 		#check for "Exiting... (End of file)" ?
 		file = shot_file
 	
@@ -399,9 +400,9 @@ def conv_thumb(file, frames):
 	thumbim = im.transform(size, Image.AFFINE, matrix , Image.BICUBIC).getdata()
 
 	data = list()
-	for i in xrange(dest_h):
+	for i in range(dest_h):
 		row = list()
-		for j in xrange(dest_w):
+		for j in range(dest_w):
 			red, green, blue = thumbim[i*dest_w+j][0], thumbim[i*dest_w+j][1], thumbim[i*dest_w+j][2]
 			pixel = (( 1 << 15)
 				| ((blue >> 3) << 10)
@@ -424,7 +425,7 @@ def conv_thumb(file, frames):
 
 	#must be sure shot_file is always named 00000001.png
 	#for batch processing
-	if ( shot_file != None ):
+	if ( shot_file is not None ):
 		if (os.path.lexists ( shot_file )):
 			os.unlink ( shot_file )
 
@@ -486,32 +487,32 @@ if options.dpg > 4:
 if options.dpg < 0:
 	options.dpg = 2
 
-test = commands.getoutput ( MPEG_STAT + " --" )
+test = subprocess.getoutput ( MPEG_STAT + " --" )
 m = re.compile ("mpeg_stat --(.*)").search(test)
 if m:
-	print m.group(0)
+	print(m.group(0))
 else:
-	print "Error:"
-	print test
+	print("Error:")
+	print(test)
 	exit (0)
-test = commands.getoutput ( MPLAYER )
+test = subprocess.getoutput ( MPLAYER )
 m = re.compile ("^MPlayer.*").search(test)
 if m:
-	print m.group(0)
+	print(m.group(0))
 else:
-	print "Error:"
-	print test
+	print("Error:")
+	print(test)
 	exit (0)
-test = commands.getoutput ( MENCODER)
+test = subprocess.getoutput ( MENCODER)
 m = re.compile ("^MEncoder.*").search(test)
 if m:
-	print m.group(0)
+	print(m.group(0))
 else:
-	print "Error:"
-	print test
+	print("Error:")
+	print(test)
 	exit (0)
-print "It seems we found all programs :)...continuing"
-print "______________________________________________"
+print("It seems we found all programs :)...continuing")
+print("______________________________________________")
 init_names()
 for file in args:
 	conv_file(file)
